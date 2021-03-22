9af1da3f7ecd2a06fcc9f4d53a088d7a280e300f
alex-battikha <56514843+alex-battikha@users.noreply.github.com>
2021-03-21 21:54:12 -0700

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,45 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score = 0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    // getName() {
+    //     var playerNameIndex = "players/player" + this.index;
+    //     var playerNameRef = database.ref('playerNameIndex');
+    //     playerNameRef.on("value", (data) => {
+    //         playerName = data.val();
+    //     });
+    // }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        });
+    }
+}
